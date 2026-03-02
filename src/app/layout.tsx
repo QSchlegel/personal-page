78e18caaef947ca3922c6cbf206d3591115e30c7
QSchlegel <quirin.schlegel@icloud.com>
2026-03-02 21:58:04 +0100

    node

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          src="https://umami-production-e1ca.up.railway.app/script.js"
+          data-website-id="a7b0e8f0-cf49-4cf7-a0e3-5b0b7137aa3b"
+        />
+      </head>
       <body className={`${displayFont.variable} ${bodyFont.variable}`}>{children}</body>
     </html>
   );
